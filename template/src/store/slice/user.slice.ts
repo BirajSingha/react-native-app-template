c7230bd4f368ac2386adbe4cf6cb9e0e30f55aca
Biraj Singha <67847698+BirajSingha@users.noreply.github.com>
2025-10-06 14:04:28 +0530

    Update user.slice.ts

--- template/src/store/slice/user.slice.ts
+++ template/src/store/slice/user.slice.ts
@@ -1,9 +1,6 @@
 import {
-  ADDITIONAL_INFO_TYPE,
   CHANGE_PASSWORD_TYPE,
   DELETE_ACCOUNT_TYPE,
-  GUIDING_MODE_RES_TYPES,
-  TRACKING_FEED_LIST,
   UPDATE_USER_INFORMATION,
 } from '@app/types';
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
@@ -15,14 +12,6 @@ interface UserState {
   updateUserInfoResponse: any;
   changePasswordResponse: any;
   deleteUserResponse: any;
-  onboardingDetails: any;
-  updateOnboardingResponse: any;
-  medicalConditionList: any;
-  deliveryInfoList: any;
-  trackingFeedAdd: any;
-  trackingFeedList: any;
-  guidingFeedRes: any;
-  guidingFeedList: any;
 }
 
 const initialState: UserState = {
@@ -32,14 +21,6 @@ const initialState: UserState = {
   updateUserInfoResponse: {},
   changePasswordResponse: {},
   deleteUserResponse: {},
-  onboardingDetails: {},
-  updateOnboardingResponse: {},
-  medicalConditionList: {},
-  deliveryInfoList: {},
-  trackingFeedAdd: {},
-  trackingFeedList: {},
-  guidingFeedRes: {},
-  guidingFeedList: {},
 };
 
 const userSlice = createSlice({
@@ -88,116 +69,6 @@ const userSlice = createSlice({
       state.error = action.payload;
     },
 
-    // Onboarding Details
-    onboardingDetailsRequest(state, action: PayloadAction) {
-      state.status = action.type;
-    },
-    onboardingDetailsSuccess(state, action) {
-      state.status = action.type;
-      state.onboardingDetails = action.payload;
-    },
-    onboardingDetailsFailure(state, action) {
-      state.status = action.type;
-      state.error = action.payload;
-    },
-
-    // Update Onboarding Details
-    updateOnboardingRequest(
-      state,
-      action: PayloadAction<ADDITIONAL_INFO_TYPE>,
-    ) {
-      state.status = action.type;
-    },
-    updateOnboardingSuccess(state, action) {
-      state.status = action.type;
-      state.updateOnboardingResponse = action.payload;
-    },
-    updateOnboardingFailure(state, action) {
-      state.status = action.type;
-      state.error = action.payload;
-    },
-
-    // Get Medical Condition List
-    medicalConditionListRequest(state, action: PayloadAction) {
-      state.status = action.type;
-    },
-    medicalConditionListSuccess(state, action) {
-      state.status = action.type;
-      state.medicalConditionList = action.payload;
-    },
-    medicalConditionListFailure(state, action) {
-      state.status = action.type;
-      state.error = action.payload;
-    },
-
-    // Get Delivery Info List
-    deliveryInfoListRequest(state, action: PayloadAction) {
-      state.status = action.type;
-    },
-    deliveryInfoListSuccess(state, action) {
-      state.status = action.type;
-      state.deliveryInfoList = action.payload;
-    },
-    deliveryInfoListFailure(state, action) {
-      state.status = action.type;
-      state.error = action.payload;
-    },
-
-    // Tracking Feed Add
-    trackingFeedAddRequest(state, action: PayloadAction) {
-      state.status = action.type;
-    },
-    trackingFeedAddSuccess(state, action) {
-      state.status = action.type;
-      state.trackingFeedAdd = action.payload;
-    },
-    trackingFeedAddFailure(state, action) {
-      state.status = action.type;
-      state.error = action.payload;
-    },
-
-    // Tracking Feed List
-    trackingFeedListRequest(state, action: PayloadAction<TRACKING_FEED_LIST>) {
-      state.status = action.type;
-    },
-    trackingFeedListSuccess(state, action) {
-      state.status = action.type;
-      state.trackingFeedList = action.payload;
-    },
-    trackingFeedListFailure(state, action) {
-      state.status = action.type;
-      state.error = action.payload;
-    },
-
-    // Guiding Feed Result
-    guidingFeedResultRequest(
-      state,
-      action: PayloadAction<GUIDING_MODE_RES_TYPES>,
-    ) {
-      state.status = action.type;
-    },
-    guidingFeedResultSuccess(state, action) {
-      state.status = action.type;
-      state.guidingFeedRes = action.payload;
-    },
-    guidingFeedResultFailure(state, action) {
-      state.status = action.type;
-      state.error = action.payload;
-    },
-
-    // guiding Feed List
-    guidingFeedListRequest(state, action: PayloadAction) {
-      state.status = action.type;
-    },
-    guidingFeedListSuccess(state, action) {
-      state.status = action.type;
-      state.guidingFeedList = action.payload;
-    },
-    guidingFeedListFailure(state, action) {
-      state.status = action.type;
-      state.error = action.payload;
-    },
-
     // Delete account
     deleteAcoountRequest(state, action: PayloadAction<DELETE_ACCOUNT_TYPE>) {
       state.status = action.type;
@@ -234,46 +105,6 @@ export const {
   changePasswordRequest,
   changePasswordSuccess,
   changePasswordFailure,
-
-  // onboarding details
-  onboardingDetailsRequest,
-  onboardingDetailsSuccess,
-  onboardingDetailsFailure,
-
-  // update onboarding details
-  updateOnboardingRequest,
-  updateOnboardingSuccess,
-  updateOnboardingFailure,
-
-  // medical condition list
-  medicalConditionListRequest,
-  medicalConditionListSuccess,
-  medicalConditionListFailure,
-
-  // delivery info list
-  deliveryInfoListRequest,
-  deliveryInfoListSuccess,
-  deliveryInfoListFailure,
-
-  // tracking feed add
-  trackingFeedAddRequest,
-  trackingFeedAddSuccess,
-  trackingFeedAddFailure,
-
-  // tracking feed list
-  trackingFeedListRequest,
-  trackingFeedListSuccess,
-  trackingFeedListFailure,
-
-  // guiding feed result
-  guidingFeedResultRequest,
-  guidingFeedResultSuccess,
-  guidingFeedResultFailure,
-
-  // guiding feed list
-  guidingFeedListRequest,
-  guidingFeedListSuccess,
-  guidingFeedListFailure,
 
   // delete account
   deleteAcoountRequest,
