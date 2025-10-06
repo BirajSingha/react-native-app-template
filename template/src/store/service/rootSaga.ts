0d5c767b80d440b4ef7224bedc22a5f22f0f92f7
Biraj Singha <67847698+BirajSingha@users.noreply.github.com>
2025-10-06 13:59:09 +0530

    Update rootSaga.ts

--- template/src/store/service/rootSaga.ts
+++ template/src/store/service/rootSaga.ts
@@ -1,8 +1,7 @@
 import {all} from 'redux-saga/effects';
 import authSaga from './auth.saga';
 import userSaga from './user.saga';
-import supportSaga from './support.saga';
 
 export default function* rootSaga() {
-  yield all([authSaga(), userSaga(), supportSaga()]);
+  yield all([authSaga(), userSaga()]);
 }
